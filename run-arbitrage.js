b89b294cbad724e8832ba1af335985cff8e7293a
jsan <ahsan.wtc@gmail.com>
2022-04-24 23:37:25 +0500

    fetching data from uniswap

--- run-arbitrage.js
+++ run-arbitrage.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const Web3 = require('web3');
+const { ChainId, Token, TokenAmount, Pair, Fetcher, WETH } = require('@uniswap/sdk');
 
 const abis = require('./abis');
 const addresses = require('./addresses');
@@ -18,28 +19,51 @@ const ETH_AMOUNT = 100;
 const AMOUNT_ETH_WEI = web3.utils.toWei(ETH_AMOUNT.toString());
 const AMOUNT_DAI_WEI = web3.utils.toWei((ETH_AMOUNT * RECENT_ETH_PRICE).toString());
 
-/* subscribing to notification whenever a new block is mined */
-/* just interested in block headers because don't need the block data  */
-web3.eth.subscribe('newBlockHeaders')
-  .on('data', async block => {
-    console.log(`new block received | block # ${block.number}`);
-    const kyberResults = await Promise.all([
-      /* rates when selling dai to buy ether */
-      kyber.methods.getExpectedRate(addresses.mainnet.tokens.dai, '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee', AMOUNT_DAI_WEI).call(),
-      /* rates when selling ehter to buy dai */
-      kyber.methods.getExpectedRate('0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee', addresses.mainnet.tokens.dai, AMOUNT_ETH_WEI).call()
-    ]);
+const DAI = new Token(ChainId.MAINNET, '0x6B175474E89094C44Da98b954EedeAC495271d0F', 18)
 
-    /* calculations with referece to dai */
-    /* to sell dai, we will be BUYing eth, rate should be 1/rate to get the value in dai */
-    const kyberRates = {
-      buy: parseFloat(1 / (kyberResults[0].expectedRate / (10 ** 18))),
-      sell: parseFloat(kyberResults[1].expectedRate / (10 ** 18))
-    };
+const init = async () => {
 
-    console.log('Kyber ETH/DAI');
-    console.log(kyberRates);
-  })
-  .on('error', error => {
-    console.log(error);
-  });
+  const [dai, weth] = await Promise.all(
+    [addresses.mainnet.tokens.dai, addresses.mainnet.tokens.weth].map(address => Fetcher.fetchTokenData(ChainId.MAINNET, address))
+  );
+
+  const daiWeth = await Fetcher.fetchPairData(DAI, WETH[DAI.chainId]);
+  // const daiWeth = await Pair.fetchData(dai, weth);
+
+  
+  /* subscribing to notification whenever a new block is mined */
+  /* just interested in block headers because don't need the block data  */
+  web3.eth.subscribe('newBlockHeaders')
+    .on('data', async block => {
+      console.log(`new block received | block # ${block.number}`);
+      const kyberResults = await Promise.all([
+        /* rates when selling dai to buy ether */
+        kyber.methods.getExpectedRate(addresses.mainnet.tokens.dai, '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee', AMOUNT_DAI_WEI).call(),
+        /* rates when selling ehter to buy dai */
+        kyber.methods.getExpectedRate('0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee', addresses.mainnet.tokens.dai, AMOUNT_ETH_WEI).call()
+      ]);
+  
+      /* calculations with referece to dai */
+      /* to sell dai, we will be BUYing eth, rate should be 1/rate to get the value in dai */
+      const kyberRates = {
+        buy: parseFloat(1 / (kyberResults[0].expectedRate / (10 ** 18))),
+        sell: parseFloat(kyberResults[1].expectedRate / (10 ** 18))
+      };
+  
+      console.log('Kyber ETH/DAI');
+      console.log(kyberRates);
+
+      const uniswapResults = await Promise.all([
+        daiWeth.getOutputAmount(new TokenAmount(dai, AMOUNT_DAI_WEI)),
+        daiWeth.getOutputAmount(new TokenAmount(weth, AMOUNT_ETH_WEI)),
+      ]);
+
+      console.log(uniswapResults);
+    })
+    .on('error', error => {
+      console.log(error);
+    });
+
+};
+
+init();
